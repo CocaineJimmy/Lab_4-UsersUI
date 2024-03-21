db2a18d85d45ada92dafe871d94d375b0f115829
sadar <sadarqsa@gmail.com>
2024-03-21 21:17:31 +0300

    Update AppDrawer.kt

--- app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
@@ -1,35 +1,47 @@
 package com.topic2.android.notes.ui.components
 
-import android.graphics.ColorFilter
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.RowScopeInstance.align
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.routing.NotesRouter
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.theme.NotesThemeSettings
 
 @Composable
 private fun AppDrawerHeader(){
-    Row(modifier = Modifier.fillMaxSize()){
+    Row(modifier = Modifier.fillMaxWidth()){
         Image(
             imageVector = Icons.Filled.Menu,
             contentDescription = "Drawer Header Icon",
-            colorFilter = androidx.compose.ui.graphics.ColorFilter
+            colorFilter = ColorFilter
                 .tint(MaterialTheme.colors.onSurface),
             modifier = Modifier.padding(16.dp)
         )
     }
+    Text(text = "Заметки",
+        modifier = Modifier
+            .align(alignment = Alignment.CenterVertically))
 }
 
 @Preview
 @Composable
 fun AppDrawerHeaderPreview(){
-    JetNotesTheme{
+    NotesTheme {
         AppDrawerHeader()
     }
 }
+
